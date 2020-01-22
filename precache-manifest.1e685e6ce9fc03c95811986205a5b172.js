c395ea73458b9392a49da599f26d00204d0330bf
seo-mouse <gda.form@gmail.com>
2020-01-22 19:52:26 +0300

    Updates

---
+++ precache-manifest.1e685e6ce9fc03c95811986205a5b172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed76810bfacec9952f28421061164e3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4b4905380dd9ebaf3143",
+    "url": "/monsters-rolodex/static/css/main.7d1c97c7.chunk.css"
+  },
+  {
+    "revision": "a4dff5b7c70ab47a72d5",
+    "url": "/monsters-rolodex/static/js/2.d612ab20.chunk.js"
+  },
+  {
+    "revision": "4b4905380dd9ebaf3143",
+    "url": "/monsters-rolodex/static/js/main.229a0ce4.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
